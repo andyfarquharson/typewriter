9d3191c06201f3731c1985a2f9c9b85f3d9a4452
Andy Farquharson <andyfarquharson@gmail.com>
2023-02-15 17:56:00 -0800

    Edit newline to output properly

--- typewriter.js
+++ typewriter.js
@@ -3,8 +3,8 @@ let duration = 1000;
 
 for (const char of sentence) {
   setTimeout(() => { 
-    process.stdout.write(char)
+    process.stdout.write(char);
   }, duration);
-  newLine();
+  process.stdout.write("\n");
   duration += 50;
 }
